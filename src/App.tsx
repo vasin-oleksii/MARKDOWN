b9ac4811e445c62b151aae2d6c5afbdc1cf9b63b
v4s1n <vasin.oleksii@gmail.com>
2024-12-18 22:01:31 +0100

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex  justify-center h-screen items-center">
+      <h1 className="text-white text-3xl">Welcom! 🥰 MarkDown</h1>
+    </div>
+  );
+}
+
+export default App;
